c7f64928a6cd22443388df25c530bf01e533274d
Sam Young <youngsam@stanford.edu>
2024-10-01 11:35:46 -0700

    add position embedding and larger minipointnet

--- point2vec/modules/pointnet.py
+++ point2vec/modules/pointnet.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from pytorch3d.ops import ball_query, knn_gather, knn_points, sample_farthest_points
 from pytorch3d.ops.utils import masked_gather
 from torch import nn
-
+import math
 
 def fill_empty_indices(idx: torch.Tensor) -> torch.Tensor:
     """
@@ -28,11 +28,12 @@ class PointcloudGrouping(nn.Module):
         self.group_size = group_size
         self.group_radius = group_radius
 
-    def forward(self, points: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, points: torch.Tensor, semantic_id: torch.Tensor | None = None) -> Tuple[torch.Tensor, torch.Tensor]:
         # points: (B, N, C)
         group_centers, _ = sample_farthest_points(
             points[:, :, :3].float(), K=self.num_groups, random_start_point=True
         )  # (B, G, 3)
+        semantic_id_groups = None
         if self.group_radius is None:
             _, idx, _ = knn_points(
                 group_centers.float(),
@@ -42,6 +43,8 @@ class PointcloudGrouping(nn.Module):
                 return_nn=False,
             )  # (B, G, K)
             groups = knn_gather(points, idx)  # (B, G, K, C)
+            if semantic_id is not None:
+                semantic_id_groups = knn_gather(semantic_id, idx)  # (B, G, K)
         else:
             _, idx, _ = ball_query(
                 group_centers.float(),
@@ -50,14 +53,77 @@ class PointcloudGrouping(nn.Module):
                 radius=self.group_radius,
                 return_nn=False,
             )  # (B, G, K)
+
+            max_filled_ratio = (~idx.eq(-1)).sum(2).eq(self.group_size).float().mean()
+            if max_filled_ratio > 0.1:
+                import warnings
+                warnings.warn(
+                    f"> {max_filled_ratio*100:.2f}% of groups contain the maximum number of points "
+                    f"({self.group_size}). This implies that there are points within the group radius "
+                    "that are not being counted. Consider increasing the group size."
+                )
             groups = masked_gather(points, fill_empty_indices(idx))  # (B, G, K, C)
+
+            if semantic_id is not None:
+                semantic_id_groups = masked_gather(semantic_id, fill_empty_indices(idx))  # (B, G, K)
 
         groups[:, :, :, :3] = groups[:, :, :, :3] - group_centers.unsqueeze(2)
         if self.group_radius is not None:
             groups = (
                 groups / self.group_radius
             )  # proposed by PointNeXT to make relative coordinates less small
-        return groups, group_centers  # (B, G, K, C), (B, G, 3)
+        return groups, group_centers, semantic_id_groups  # (B, G, K, C), (B, G, 3), (B, G, K)
+
+
+class PositionEmbeddingCoordsSine(nn.Module):
+    """Similar to transformer's position encoding, but generalizes it to
+    arbitrary dimensions and continuous coordinates.
+
+    Args:
+        n_dim: Number of input dimensions, e.g. 2 for image coordinates.
+        d_model: Number of dimensions to encode into
+        temperature:
+        scale:
+    """
+
+    def __init__(
+        self, n_dim: int = 1, d_model: int = 256, temperature=10000, scale=None
+    ):
+        super().__init__()
+
+        self.n_dim = n_dim
+        self.num_pos_feats = d_model // n_dim // 2 * 2
+        self.temperature = temperature
+        self.padding = d_model - self.num_pos_feats * self.n_dim
+
+        if scale is None:
+            scale = 1.0
+        self.scale = scale * 2 * math.pi
+
+    def forward(self, xyz: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            xyz: Point positions (*, d_in)
+
+        Returns:
+            pos_emb (*, d_out)
+        """
+        assert xyz.shape[-1] == self.n_dim
+
+        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=xyz.device)
+        dim_t = self.temperature ** (
+            2 * torch.div(dim_t, 2, rounding_mode="trunc") / self.num_pos_feats
+        )
+
+        xyz = xyz * self.scale
+        pos_divided = xyz.unsqueeze(-1) / dim_t
+        pos_sin = pos_divided[..., 0::2].sin()
+        pos_cos = pos_divided[..., 1::2].cos()
+        pos_emb = torch.stack([pos_sin, pos_cos], dim=-1).reshape(*xyz.shape[:-1], -1)
+
+        # Pad unused dimensions with zeros
+        pos_emb = nn.functional.pad(pos_emb, (0, self.padding))
+        return pos_emb
 
 
 class MiniPointNet(nn.Module):
@@ -89,6 +155,38 @@ class MiniPointNet(nn.Module):
         feature_global = torch.max(feature, dim=2).values  # (B, feature_dim)
         return feature_global
 
+class LargePointNet(nn.Module):
+    def __init__(self, channels: int, feature_dim: int):
+        super().__init__()
+        self.first_conv = nn.Sequential(
+            nn.Conv1d(channels, 256, 1, bias=False),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(256, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 1024, 1),
+        )
+
+        self.second_conv = nn.Sequential(
+            nn.Conv1d(2048, 2048, 1, bias=False),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(2048, feature_dim, 1),
+        )
+
+    def forward(self, points) -> torch.Tensor:
+        # points: (B, N, C)
+        feature = self.first_conv(points.transpose(2, 1))  # (B, 256, N)
+        feature_global = torch.max(feature, dim=2, keepdim=True).values  # (B, 256, 1)
+        # concating global features to each point features
+        feature = torch.cat(
+            [feature_global.expand(-1, -1, feature.shape[2]), feature], dim=1
+        )  # (B, 512, N)
+        feature = self.second_conv(feature)  # (B, feature_dim, N)
+        feature_global = torch.max(feature, dim=2).values  # (B, feature_dim)
+        return feature_global
+
 
 class PointcloudTokenizer(nn.Module):
     def __init__(
@@ -97,23 +195,31 @@ class PointcloudTokenizer(nn.Module):
         group_size: int,
         group_radius: float | None,
         token_dim: int,
+        num_channels: int,
+        embedding_type: str = "mini",
     ) -> None:
         super().__init__()
         self.token_dim = token_dim
         self.grouping = PointcloudGrouping(
             num_groups=num_groups, group_size=group_size, group_radius=group_radius
         )
-        self.embedding = MiniPointNet(3, token_dim)
 
-    def forward(self, points: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        # points: (B, N, 3)
+        if embedding_type == "mini":
+            self.embedding = MiniPointNet(num_channels, token_dim)
+        elif embedding_type == "large":
+            self.embedding = LargePointNet(num_channels, token_dim)
+        else:
+            raise ValueError(f"Unknown embedding type: {embedding_type}")
+
+    def forward(self, points: torch.Tensor, semantic_id: torch.Tensor | None = None) -> Tuple[torch.Tensor, torch.Tensor]:
+        # points: (B, N, num_channels)
         group: torch.Tensor
         group_center: torch.Tensor
         tokens: torch.Tensor
 
-        group, group_center = self.grouping(points)  # (B, G, K, C), (B, G, 3)
+        group, group_center, semantic_id_groups = self.grouping(points, semantic_id)  # (B, G, K, C), (B, G, 3), (B, G, K)
         B, G, S, C = group.shape
         tokens = self.embedding(group.reshape(B * G, S, C)).reshape(
             B, G, self.token_dim
         )  # (B, G, C')
-        return tokens, group_center
+        return tokens, group_center, semantic_id_groups
