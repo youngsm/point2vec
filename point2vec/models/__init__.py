24128f0289453336b9eb1f2eeacaa613c0a37618
Sam Young <youngsam@stanford.edu>
2024-12-12 11:25:28 -0800

    add point-mae & ppn to model __init__

--- point2vec/models/__init__.py
+++ point2vec/models/__init__.py
@@ -2,3 +2,5 @@ from .classification import Point2VecClassification
 from .part_segmentation import Point2VecPartSegmentation
 from .keypoint_generation import Point2VecKeyPointGeneration
 from .point2vec import Point2Vec
+from .pointmae import PointMAE
+from .ppn import Point2VecPointProposalNetwork
